aa9aad26b40c3682126faa0fd7f7432ba588c2db
Flavien Darche <flavien1801@protonmail.com>
2022-01-17 23:48:29 +0100

    update base58 prefixes

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -101,10 +101,10 @@ public:
          * The characters are rarely used upper ASCII, not valid as UTF-8, and produce
          * a large 32-bit integer with any alignment.
          */
-        pchMessageStart[0] = 0xfb;
-        pchMessageStart[1] = 0xc0;
-        pchMessageStart[2] = 0xb6;
-        pchMessageStart[3] = 0xdb;
+        pchMessageStart[0] = 0xd0;
+        pchMessageStart[1] = 0xe1;
+        pchMessageStart[2] = 0xf5;
+        pchMessageStart[3] = 0xec;
         nDefaultPort = 1801;
         nPruneAfterHeight = 100000;
         m_assumed_blockchain_size = 22;
@@ -125,12 +125,12 @@ public:
         vSeeds.emplace_back("dnsseed.breekcointools.com");
         vSeeds.emplace_back("dnsseed.breekcoinpool.org");
 
-        base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,48);
-        base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1,5);
+        base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,25);
+        base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1,25);
         base58Prefixes[SCRIPT_ADDRESS2] = std::vector<unsigned char>(1,50);
         base58Prefixes[SECRET_KEY] =     std::vector<unsigned char>(1,176);
-        base58Prefixes[EXT_PUBLIC_KEY] = {0x04, 0x88, 0xB2, 0x1E};
-        base58Prefixes[EXT_SECRET_KEY] = {0x04, 0x88, 0xAD, 0xE4};
+        base58Prefixes[EXT_PUBLIC_KEY] = {0x18, 0x88, 0xB2, 0x1E};
+        base58Prefixes[EXT_SECRET_KEY] = {0x18, 0x88, 0xAD, 0xE4};
 
         bech32_hrp = "ltc";
 
